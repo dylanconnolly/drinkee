fb0dfbe5b21e2a9c9de5a52e136cca8af2dce6f3
Dylan Connolly <dconnolly@Dylans-MacBook-Pro.local>
2023-01-30 20:56:48 -0700

    add mongo

--- main.go
+++ main.go
@@ -1,10 +1,18 @@
 package main
 
 import (
+	"fmt"
+
+	"github.com/dylanconnolly/drinkee/mongo"
 	"github.com/dylanconnolly/drinkee/router"
 )
 
 func main() {
+	fmt.Println("connecting to mongo...")
+	err := mongo.CreateMongoConnection()
+	if err != nil {
+		fmt.Printf("error connecting to mongo: %s", err)
+	}
 	r := router.CreateRouter()
 
 	r.Run() // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
