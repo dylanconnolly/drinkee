04f1fae7af369ae0cb697c5cca90432b390263f9
Dylan Connolly <dylanconnolly145@gmail.com>
2023-03-16 23:11:29 -0600

    add ingredients and drink ingredients route

--- router/router.go
+++ router/router.go
@@ -14,6 +14,16 @@ type Drink struct {
 	Instructions string `json:"instructions"`
 }
 
+type Ingredient struct {
+	ID   int    `json:"id"`
+	Name string `json:"name"`
+}
+
+type DrinkIngredient struct {
+	Name        string `json:"name"`
+	Measurement string `json:"measurement"`
+}
+
 type BaseRouter struct {
 	db *sqlx.DB
 }
@@ -40,6 +50,45 @@ func (br *BaseRouter) getDrinkByID(c *gin.Context) {
 	c.IndentedJSON(http.StatusOK, drink)
 }
 
+func (br *BaseRouter) getDrinkIngredients(c *gin.Context) {
+	drinkID := c.Param("id")
+	var drinkIngredients []DrinkIngredient
+
+	queryStr := `
+	SELECT ingredients.name, drink_ingredients.measurement
+	FROM drink_ingredients JOIN ingredients ON ingredients.id = drink_ingredients.ingredient_id
+	WHERE drink_id=$1`
+
+	err := br.db.Select(&drinkIngredients, queryStr, drinkID)
+	if err != nil {
+		c.JSON(http.StatusOK, "No drink ingredients for that drink")
+		return
+	}
+
+	c.IndentedJSON(http.StatusOK, drinkIngredients)
+}
+
+func (br *BaseRouter) getIngredients(c *gin.Context) {
+	var ingredients []Ingredient
+
+	br.db.Select(&ingredients, "SELECT * FROM ingredients")
+
+	c.IndentedJSON(http.StatusOK, ingredients)
+}
+
+func (br *BaseRouter) getIngredientByID(c *gin.Context) {
+	id := c.Param("id")
+	var ingredient Ingredient
+
+	err := br.db.Get(&ingredient, "SELECT * FROM ingredients WHERE id=$1", id)
+	if err != nil {
+		c.JSON(http.StatusOK, "No drink with that id")
+		return
+	}
+
+	c.IndentedJSON(http.StatusOK, ingredient)
+}
+
 func CreateNewRouter(db *sqlx.DB) *gin.Engine {
 	br := &BaseRouter{
 		db: db,
@@ -58,6 +107,15 @@ func CreateNewRouter(db *sqlx.DB) *gin.Engine {
 	})
 	router.GET("/drinks/:id", func(c *gin.Context) {
 		br.getDrinkByID(c)
+	})
+	router.GET("drinks/:id/ingredients", func(c *gin.Context) {
+		br.getDrinkIngredients(c)
+	})
+	router.GET("/ingredients", func(c *gin.Context) {
+		br.getIngredients(c)
+	})
+	router.GET("/ingredients/:id", func(c *gin.Context) {
+		br.getIngredientByID(c)
 	})
 
 	return router
